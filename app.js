ce86801a90ac46c99f7ef0bbfd30d6e52dd4aff2
Nitin Thomas <nitin.tomas31@gmail.com>
2025-02-16 14:47:33 +0530

    Your descriptive commit message

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const productRouter = require("./routes/productRouter");
+const movieRouter = require("./routes/movieRouter");
 
-app.use("/books", productRouter);
+app.use(express.json()); // Middleware to parse JSON requests
+app.use("/movies", movieRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
--- products.js
+++ products.js
@@ -1,12 +1,27 @@
-const booksList = [
+const movies = [
   {
     id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
+    title: "Inception",
+    genre: "Sci-Fi",
+    releaseYear: 2010,
+    rating: 8.8,
   },
-  { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-  { id: 3, title: "1984", author: "George Orwell", year: 1949 },
+  {
+    id: 2,
+    title: "The Dark Knight",
+    genre: "Action",
+    releaseYear: 2008,
+    rating: 9.0,
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    genre: "Sci-Fi",
+    releaseYear: 2014,
+    rating: 8.6,
+  },
+  { id: 4, title: "Titanic", genre: "Romance", releaseYear: 1997, rating: 7.8 },
+  { id: 5, title: "Avatar", genre: "Sci-Fi", releaseYear: 2009, rating: 7.9 },
 ];
 
-module.exports = booksList;
+module.exports = movies;
